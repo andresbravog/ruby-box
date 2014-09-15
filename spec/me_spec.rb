3067f2ee7b63c8747359007f7aac6c8c5db79e86
andresbravog <andresbravog@gmail.com>
2014-09-15 09:12:11 +0200

    Added specs for query params in users and me endpoints

--- spec/me_spec.rb
+++ spec/me_spec.rb
@@ -17,4 +17,20 @@ describe '/users/me' do
     me.instance_of?(RubyBox::User).should be_true
   end
 
+  context 'with fields query param given' do
+    let(:me_with_role_json) do
+      hash = JSON.parse(@me_json)
+      hash['role'] = 'coadmin'
+      hash.to_json
+    end
+
+    before do
+      stub_request(:get, /#{RubyBox::API_URL}\/users\/me\?fields=role/).to_return(body: me_with_role_json,
+                                                                                  :status => 200)
+    end
+    subject { @client.me(fields: 'role') }
+
+    it { should be_a RubyBox::User }
+    its(:role) { should eq 'coadmin' }
+  end
 end
